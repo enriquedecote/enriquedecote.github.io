0f45dbfc994aca4758bf93ed159f69a4fc21251b
enriquedecote <enriquedecote@gmail.com>
2022-11-01 12:21:36 +0000

    added timeline to ventures

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,10 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  content: [
+    "./node_modules/flowbite/**/*.js"
+  ]
 }
